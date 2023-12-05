e8c312674c965f70faefce04d927f3813cd0f34a
EduardoSalasG <eduardo.salasgo@correoaiep.cl>
2023-12-04 22:45:17 -0300

    Comenzando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //   {
+    //     path: 'heroes',
+    //     loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    //   },
+    // {
+    //     path: 'login',
+    //     component: Error404PageComponent
+    //   },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '404',
+        component: Error404PageComponent
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '404'
+    }];
